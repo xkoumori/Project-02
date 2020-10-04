c158a7ac4fc1d8d2ba288b767d24cdc5c7d5ecbb
Sammie Ortiz <ortizxsam@gmail.com>
2020-10-03 23:22:35 -0500

    Eighth commit

--- backup.sh
+++ backup.sh
@@ -6,6 +6,6 @@ user=$(whoami)
 input=/home/$user
 output=/tmp/${user}_home_$(date +%Y-%m-%d_%H%M%S).tar.gz
 
-tar -czf $output $input
+tar -czf $output $input 2> /dev/null
 echo "Backup of $input completed! Details about the output backup file:"
 ls -l $output
