94280dec0a55411931f27bd245d63e18829add4f
DylanJ513 <71000101+DylanJ513@users.noreply.github.com>
2020-10-31 06:36:40 -0500

    Update while.sh

--- while.sh
+++ while.sh
@@ -5,3 +5,5 @@ while [ $counter -lt 3 ]; do
 	let counter+=1
 	echo $counter
 done
+//The while loop loops through a block of code as long as a specified condition is true
+//counter is set to the value 2, then the code in the brackets sets the condition.
