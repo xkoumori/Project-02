9ba2d5a324982123b365e061087d258d494b5b8a
DylanJ513 <71000101+DylanJ513@users.noreply.github.com>
2020-10-31 06:31:11 -0500

    Update if_else.sh

--- if_else.sh
+++ if_else.sh
@@ -8,3 +8,5 @@ if [ $num_a -lt $num_b ]; then
 else
 	echo "$num_a is greater than $num_b!"
 fi
+//The if/else statement executes a block of code if a specified condition is true. If the condition is false, another block of code can be executed.
+//In this case, the two integers used for the if/else statement is a and b, with the values of 400 and 200 assigned to them respectivley.
